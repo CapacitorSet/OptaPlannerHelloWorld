c0879344af61a0500083ac79789547504b36b0d9
Capacitor Set <CapacitorSet@users.noreply.github.com>
2016-01-15 09:00:30 +0100

    Commentato un attimino

--- src/main/java/org/btrg/optaplannerhellotest/HelloApp.java
+++ src/main/java/org/btrg/optaplannerhellotest/HelloApp.java
@@ -7,35 +7,46 @@ import org.optaplanner.core.impl.score.director.ScoreDirector;
 import org.optaplanner.core.impl.score.director.ScoreDirectorFactory;
 
 public class HelloApp {
-	public static final String SOLVER_CONFIG_XML = "org/btrg/optaplannerhellotest/solver/helloCounterSolverConfig.xml";
-	public static ScoreDirector scoreDirector;
-	public static int printCount = 0;
+    public static final String SOLVER_CONFIG_XML = "org/btrg/optaplannerhellotest/solver/helloCounterSolverConfig.xml";
+    public static ScoreDirector scoreDirector;
+    public static int printCount = 0;
 
-	public static void main(String[] args) {
-		Solver solver = SolverFactory.createFromXmlResource(SOLVER_CONFIG_XML)
-				.buildSolver();
-		ScoreDirectorFactory scoreDirectorFactory = solver
-				.getScoreDirectorFactory();
-		scoreDirector = scoreDirectorFactory.buildScoreDirector();
-		HelloSolution helloSolution = new HelloSolution();
-		scoreDirector.setWorkingSolution(helloSolution);
-		System.out.println("\n\nTRYING COUNT AT:");
-		solver.solve(helloSolution);
-		HelloSolution bestHelloSolution = (HelloSolution) solver
-				.getBestSolution();
-		System.out
-				.println("\n\nBEST SOLUTION HAS EXACTLY "
-						+ bestHelloSolution.getTaskList().get(0).getCount()
-						+ " HELLOS");
-	}
+    public static void main(String[] args) {
+        /*  Crea un solver a partire dalla definizione XML.
+            Il file dice che x.domain.HelloSolution contiene la SolutionClass,
+            e x.domain.HelloEntity contiene l'EntityClass.
+        
+            La Score Directory Factory dice che lo score è HARD_SOFT, definito in helloCounterScoreRules.drl.
+        
+            Poi dice che termina quando ha fatto 10 secondi di calcolo.
+        
+            Il resto, boh.
+        */
+        
+        Solver solver = SolverFactory.createFromXmlResource(SOLVER_CONFIG_XML).buildSolver();
 
-	public static String getSpacerFeed() {
-		printCount++;
-		if (printCount == 25) {
-			printCount = 0;
-			return "\n";
-		} else {
-			return " ";
-		}
-	}
+        // Crea lo ScoreDirector a partire dalla Factory definita nel solver
+        ScoreDirectorFactory scoreDirectorFactory = solver.getScoreDirectorFactory();
+        scoreDirector = scoreDirectorFactory.buildScoreDirector();
+        
+        // x.domain.HelloSolution (è una SolutionClass)
+        HelloSolution helloSolution = new HelloSolution();
+        scoreDirector.setWorkingSolution(helloSolution);
+        
+        System.out.println("\n\nTRYING COUNT AT:");
+        solver.solve(helloSolution);
+        
+        HelloSolution bestHelloSolution = (HelloSolution) solver.getBestSolution();
+        System.out.println("\n\nBEST SOLUTION HAS EXACTLY " + bestHelloSolution.getTaskList().get(0).getCount() + " HELLOS");
+    }
+
+    public static String getSpacerFeed() {
+        printCount++;
+        if (printCount == 25) {
+            printCount = 0;
+            return "\n";
+        } else {
+            return " ";
+        }
+    }
 }
--- src/main/java/org/btrg/optaplannerhellotest/domain/HelloSolution.java
+++ src/main/java/org/btrg/optaplannerhellotest/domain/HelloSolution.java
@@ -15,62 +15,64 @@ import org.optaplanner.core.impl.score.director.ScoreDirectorFactory;
 
 @PlanningSolution
 public class HelloSolution implements Solution<HardSoftScore>, Serializable {
-	List<HelloEntity> hellos;
-	List<Integer> helloCounts;
-	private final int HELLO_MAX = 25;
-	private HardSoftScore score;
+    // Una lista di HelloEntities
+    List<HelloEntity> hellos;
+    // Lista di interi
+    List<Integer> helloCounts;
+    private final int HELLO_MAX = 25;
+    private HardSoftScore score;
 
-	public HelloSolution() {
-		populateHellos();
-		populateHelloCounts();
-	}
+    public HelloSolution() {
+        // La riga sotto equivale a hellos = [new HelloEntity(17)]
+        populateHellos();
+        // helloCounts = [0..HELLO_MAX]
+        populateHelloCounts();
+    }
 
-	@PlanningEntityCollectionProperty
-	public List<HelloEntity> getTaskList() {
-		if (hellos == null) {
-			populateHellos();
-		}
-		return hellos;
-	}
+    @PlanningEntityCollectionProperty
+    public List<HelloEntity> getTaskList() {
+        if (hellos == null) {
+                populateHellos();
+        }
+        return hellos;
+    }
 
-	@ValueRangeProvider(id = "helloCountRange")
-	public List<Integer> getHelloCountList() {
-		return helloCounts;
-	}
+    @ValueRangeProvider(id = "helloCountRange")
+    public List<Integer> getHelloCountList() {
+        return helloCounts;
+    }
 
-	public Collection<? extends Object> getProblemFacts() {
-		List<Object> facts = new ArrayList<Object>();
-		// nothing to add because the only facts are already added automatically
-		// by planner
-		return facts;
-	}
+    public Collection<? extends Object> getProblemFacts() {
+        List<Object> facts = new ArrayList<Object>();
+        // nothing to add because the only facts are already added automatically
+        // by planner
+        return facts;
+    }
 
-	public HardSoftScore getScore() {
-		if(null==HelloApp.scoreDirector){
-			System.out.println("WOOPS YOU SHOULD EXPECT SOME ISSUES HERE");
-		}
-		HardSoftScore hardSoftScore = (HardSoftScore)HelloApp.scoreDirector.calculateScore();
-		System.out.println("SCORE "+ hardSoftScore.toString() );
-		return hardSoftScore;
-	}
+    public HardSoftScore getScore() {
+        if (null==HelloApp.scoreDirector)
+            System.out.println("WOOPS YOU SHOULD EXPECT SOME ISSUES HERE");
+        HardSoftScore hardSoftScore = (HardSoftScore)HelloApp.scoreDirector.calculateScore();
+        System.out.println("SCORE "+ hardSoftScore.toString() );
+        return hardSoftScore;
+    }
 
-	public void setScore(HardSoftScore arg0) {
-		this.score = score;
-	}
+    public void setScore(HardSoftScore arg0) {
+        this.score = score;
+    }
 
-	private void populateHellos() {
-		if (null == hellos) {
-			hellos = new ArrayList<HelloEntity>();
-			hellos.add(new HelloEntity(17));
-		}
-	}
+    private void populateHellos() {
+        if (null == hellos) {
+            hellos = new ArrayList<HelloEntity>();
+            hellos.add(new HelloEntity(17));
+        }
+}
 
-	private void populateHelloCounts() {
-		if (null == helloCounts) {
-			helloCounts = new ArrayList<Integer>();
-			for (int i = 0; i < HELLO_MAX; i++) {
-				helloCounts.add(new Integer(i));
-			}
-		}
-	}
+    private void populateHelloCounts() {
+        if (null == helloCounts) {
+            helloCounts = new ArrayList<Integer>();
+            for (int i = 0; i < HELLO_MAX; i++)
+                helloCounts.add(i);
+        }
+    }
 }
